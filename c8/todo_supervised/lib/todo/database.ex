67c76bc7f46626d994bb5084c6fdf7956bc690cf
Miguel Palhas <mpalhas@gmail.com>
2016-08-15 15:09:44 +0100

    Finishing chapter 8

---
+++ c8/todo_supervised/lib/todo/database.ex
@@ -0,0 +1,35 @@
+defmodule Todo.Database do
+  use GenServer
+
+  @pool_size 3
+
+  def start_link(db_folder) do
+    IO.puts "Starting database server"
+
+    GenServer.start_link(__MODULE__, db_folder, name: :database_server)
+  end
+
+  def store(key, data) do
+    GenServer.call(:database_server, {:get_worker, key})
+    |> GenServer.cast({:store, key, data})
+  end
+
+  def get(key) do
+    result = GenServer.call(:database_server, {:get_worker, key})
+    |> GenServer.call({:store, key})
+  end
+
+  def init(db_folder) do
+    workers = Enum.reduce(
+      1..3,
+      %{},
+      &(Map.put(&2, &1, Todo.DatabaseWorker.start(db_folder)))
+    )
+
+    {:ok, workers}
+  end
+
+  def handle_call({:get_worker, key}, _, workers) do
+    {:reply, Map.fetch(workers, :erlang.phash2(key, @pool_size)), workers}
+  end
+end
---
+++ c8/todo_supervised/lib/todo/server.ex
@@ -0,0 +1,39 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start_link(name) do
+    GenServer.start_link(Todo.Server, name)
+  end
+
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new}}
+  end
+
+  def add_entry(server, entry) do
+    GenServer.cast(server, {:add, entry})
+  end
+
+  def update_entry(server, entry_id, updater_fun) do
+    GenServer.cast(server, {:update, entry_id, updater_fun})
+  end
+
+  def entries(server, date) do
+    GenServer.call(server, {:entries, date})
+  end
+
+  def handle_cast({:add, entry}, {name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
+  end
+
+  def handle_cast({:update, entry_id, updater_fun}, {name, todo_list}) do
+    new_state = Todo.List.update_entry(todo_list, entry_id, updater_fun)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
+  end
+
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
+  end
+end
