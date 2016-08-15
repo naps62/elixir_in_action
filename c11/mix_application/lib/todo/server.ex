420cce9967c76cecb8433bc92cdf7041327166f6
Miguel Palhas <mpalhas@gmail.com>
2016-08-15 18:04:30 +0100

    Finishing chapter 11

---
+++ c11/mix_application/lib/todo/server.ex
@@ -0,0 +1,49 @@
+defmodule Todo.Server do
+  use GenServer
+
+  def start_link(name) do
+    IO.puts "Starting to-do server for #{name}"
+
+    GenServer.start_link(__MODULE__, name, name: via_tuple(name))
+  end
+
+  defp via_tuple(name) do
+    {:via, :gproc, {:n, :l, {:todo_server, name}}}
+  end
+
+  def whereis(name) do
+    :gproc.whereis_name(:n, :l, {:todo_server, name})
+  end
+
+  def init(name) do
+    {:ok, {name, Todo.Database.get(name) || Todo.List.new}}
+  end
+
+  def add_entry(server, entry) do
+    GenServer.cast(server, {:add, entry})
+  end
+
+  def update_entry(server, entry_id, updater_fun) do
+    GenServer.cast(server, {:update, entry_id, updater_fun})
+  end
+
+  def entries(server, date) do
+    GenServer.call(server, {:entries, date})
+  end
+
+  def handle_cast({:add, entry}, {name, todo_list}) do
+    new_state = Todo.List.add_entry(todo_list, entry)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
+  end
+
+  def handle_cast({:update, entry_id, updater_fun}, {name, todo_list}) do
+    new_state = Todo.List.update_entry(todo_list, entry_id, updater_fun)
+    Todo.Database.store(name, new_state)
+    {:noreply, {name, new_state}}
+  end
+
+  def handle_call({:entries, date}, _, {name, todo_list}) do
+    {:reply, Todo.List.entries(todo_list, date), {name, todo_list}}
+  end
+end
---
+++ c11/web_application/lib/todo/cache.ex
@@ -0,0 +1,35 @@
+defmodule Todo.Cache do
+  use GenServer
+
+  def start_link do
+    IO.puts "Starting to-do cache"
+
+    GenServer.start_link(__MODULE__, nil, name: :todo_cache)
+  end
+
+  def server_process(todo_list_name) do
+    case Todo.Server.whereis(todo_list_name) do
+      :undefined ->
+        # There's no to-do server, so we'll issue a request to the cache process
+        GenServer.call(:todo_cache, {:server_process, todo_list_name})
+
+      pid -> pid
+    end
+  end
+
+  def init(_) do
+    {:ok, HashDict.new}
+  end
+
+  def handle_call({:server_process, todo_list_name}, _, state) do
+    todo_server_pid = case Todo.Server.whereis(todo_list_name) do
+      :undefined ->
+        {:ok, pid} = Todo.ServerSupervisor.start_child(todo_list_name)
+        pid
+
+      pid -> pid
+    end
+
+    {:reply, todo_server_pid, state}
+  end
+end
